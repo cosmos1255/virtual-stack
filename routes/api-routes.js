3bef03bacf72d8745fd594791a205798fb7f5783
Jacob Thomas <jmthomas9619@gmail.com>
2019-10-28 22:13:54 -0400

    add: created a post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,14 +3,21 @@
 // controllers
 // middleware
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// function(req, res) = (req, res) =>
 
 router.get('/', function(req, res){
     res.json({
         status: "API WORKING",
         message: "HERE IS THE API MESSAGE"
       });
+});
+
+router.post('/', function(req, res){
+  console.log(req.body)
+  res.send({ message : 'ok'})
 });
 
 module.exports = router;
