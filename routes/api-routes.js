83f66377ba56cb2511c8214627344f96ec5854f8
Jacob Thomas <jmthomas9619@gmail.com>
2019-10-28 22:30:06 -0400

    fix: added body parser for testing post requests

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,20 +3,24 @@
 // controllers
 // middleware
 
+// function(req, res) = (req, res) =>
+
 const express = require('express');
 const router = express.Router();
 
-// function(req, res) = (req, res) =>
+// body parser - used for testing post request via terminal
+const bodyParser = require('body-parser')
+router.use(bodyParser.json());
 
 router.get('/', function(req, res){
-    res.json({
-        status: "API WORKING",
-        message: "HERE IS THE API MESSAGE"
-      });
+  res.json({
+    status: "API WORKING",
+    message: "HERE IS THE API MESSAGE"
+  });
 });
 
 router.post('/', function(req, res){
-  console.log(req.body)
+  console.log(req.body);
   res.send({ message : 'ok'})
 });
 
