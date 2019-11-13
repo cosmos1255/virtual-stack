a4266ebc11c948ef9999687556b61f2cfb9bd2ef
Jacob Thomas <jmthomas9619@gmail.com>
2019-11-13 01:18:24 -0500

    chore: added, deleted, and updated business card

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,6 +16,17 @@ const controller = function(req, res) {
   });
 };
 
+// fake contact controller
+const addcontact = function(req, res){
+  res.json({
+    // id : String,
+    name: "JAKE",
+    address: "University BLVD",
+    phoneNumber: "23456789",
+    email: "EMail"
+  });
+};
+
 // testing middleware
 const logging = function(req, res, next) {
   console.log("logging");
@@ -79,13 +90,43 @@ router.get("/user/:id/card", async function(req, res){
 router.put('/user/:id', async function(req, res){
   try{
     var conditions = { _id: req.params.id }
-    var update = { "$set": {"businessCard": businessCard} }
-    Users.findOneAndUpdate(conditions, update)
+    const doc = await Users.findOneAndUpdate(conditions, req.body)
+
+    res.status(200).json({data: doc})
   }
-  
   catch (err){
     res.send({ error: err.message})
   }
+})
+
+// delete a business card
+router.delete('/user/:id', async function(req, res){
+  try{
+    var conditions = { _id: req.params.id }
+    const doc = await Users.findOneAndRemove(conditions, req.body)
+
+    res.status(200).json({data: doc})
+  }
+  catch (err){
+    res.send({ error: err.message})
+  }
+})
+
+// add a business card
+router.post('/user/:id', async function(req, res){
+  try
+  {
+    const user = await Users.findById(req.params.id)
+
+    user.businessCard.push(req.body)
+    await user.save()
+    res.json(req.body)
+  }
+  catch(err)
+  {
+    res.send({error: err})
+  }
+
 })
 
 // signup
