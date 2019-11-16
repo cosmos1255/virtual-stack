30d17ab8a75b5fed1a04aadf9dcf5bed7657b731
Jay Adams <46606151+jwadams26@users.noreply.github.com>
2019-11-16 15:45:59 -0500

    adding connections.js
    connections.js will be a library that stores async functions that talk to the api, not finished

---
+++ connection.js
@@ -0,0 +1,30 @@
+async function log_in(username, password) {
+  const response = await fetch('/api/signin', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${btoa(`${username}:${password}`)}`
+    }
+  });
+  
+  const content = response.headers.get('Content-Type') && response.headers.get('Content-Type').includes('json') ? await response.json() : await response.text();
+
+  if (response.status >= 400) throw content;
+  
+  return content;
+}
+
+async function sign_up(username, password, card) {
+  const response = await fetch('/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username, password})
+  });
+  
+  const content = response.headers.get('Content-Type') && response.headers.get('Content-Type').includes('json') ? await response.json() : await response.text();
+
+  if (response.status >= 400) throw content;
+   
+  return content;
+}
