9df4ea03f16dbf7f2964e9e752ed3bf247de8639
Jacob <jmthomas9619@gmail.com>
2019-12-01 13:42:07 -0600

    fix: added cors module

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+var cors = require('cors')
+
 const apiRoutes = require("./routes/api-routes");
 
 // setup static files
 app.use(express.static('public', { dotfiles: 'allow' }));
 app.use(express.static('node_modules', { dotfiles: 'allow' }));
+app.use(cors())
 
 var port = 3000;
 
@@ -16,7 +19,7 @@ app.get('/', function (req, res) {
 app.use('/api', apiRoutes);
 
 app.listen(process.env.PORT || port, function () {
-  console.log('Example app listening on port ' + port);
+  console.log('CORS-enabled web server listening on port ' + port)
 });
 
 // https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
