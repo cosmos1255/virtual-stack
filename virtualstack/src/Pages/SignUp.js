5fe7b7a1f8dc3be2d14fc625f806c27fca00e8a4
Blake Robertson <42613658+blakewrobertson@users.noreply.github.com>
2019-12-06 00:54:18 -0500

    Update SignUp.js

--- virtualstack/src/Pages/SignUp.js
+++ virtualstack/src/Pages/SignUp.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Form from "react-bootstrap/Form";
 import { Col } from "react-bootstrap";
 import MaskedFormControl from "react-bootstrap-maskedinput";
-import Button from "react-bootstrap/button";
+import Button from "react-bootstrap/Button";
 import SmallLogo from "./smallVSlogo.png";
 
 class SignUp extends Component {
